be83fc9e6c9e6dc6182676cf5842acf685d53810
Serhii Surnin <ssaqwer+github@gmail.com>
2026-05-28 03:15:53 +0300

    Fixed issue with icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-export const metadata = { title: 'Calculator' };
+export const metadata = { title: 'Calculator', icons: { icon: '/favicon.svg' } };
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
